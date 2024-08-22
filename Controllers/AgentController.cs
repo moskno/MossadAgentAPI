69288a4331dd5e5ad84b19ec3e75ae00ab8e0fbc
Moshe <moskno1@gmail.com>
2024-08-22 13:10:18 +0300

    update agent move completed

--- Controllers/AgentController.cs
+++ Controllers/AgentController.cs
@@ -68,20 +68,7 @@ namespace MossadAgentAPI.Controllers
 
             status = StatusCodes.Status200OK;
             return StatusCode(status, HttpUtils.Response(status, new { agent = agent }));
-
         }
-
-        //private static readonly Dictionary<string, Action<Location>> DirectionActions = new Dictionary<string, Action<Location>>
-        //    {
-        //        { "nw", loc => { loc.y -= 1; loc.x -= 1; } },
-        //        { "n", loc => loc.y -= 1 },
-        //        { "ne", loc => { loc.y -= 1; loc.x += 1; } },
-        //        { "w", loc => loc.x -= 1 },
-        //        { "e", loc => loc.x += 1 },
-        //        { "sw", loc => { loc.y += 1; loc.x -= 1; } },
-        //        { "s", loc => loc.y += 1 },
-        //        { "se", loc => { loc.y += 1; loc.x += 1; } }
-        //    };
 
         [HttpPut("{id}/move")]
         [Produces("application/json")]
@@ -92,6 +79,10 @@ namespace MossadAgentAPI.Controllers
             {
                 return BadRequest("The direction field is required.");
             }
+            if (!DirectionService.DirectionActions.ContainsKey(direction))
+            {
+                return BadRequest("Invalid direction specified.");
+            }
             int status;
             Agent agent = await this._context.agents.Include(ag => ag.location).FirstOrDefaultAsync(ag => ag.Id == id);
             if (agent == null)
@@ -99,47 +90,7 @@ namespace MossadAgentAPI.Controllers
                 status = StatusCodes.Status404NotFound;
                 return StatusCode(status, HttpUtils.Response(status, "agent not found"));
             }
-            switch (direction)
-            {
-                case "nw":
-                    direction = "nw";
-                    agent.location.y -= 1;
-                    agent.location.x -= 1;
-                    break;
-                case "n":
-                    direction = "n";
-                    agent.location.y -= 1;
-                        break;
-                case "ne":
-                    direction = "ne";
-                    agent.location.y -= 1;
-                    agent.location.x += 1;
-                    break;
-                case "w":
-                    direction = "w";
-                    agent.location.x -= 1;
-                    break;
-                case "e":
-                    direction = "e";
-                    agent.location.x += 1;
-                    break;
-                case "sw":
-                    direction = "sw";
-                    agent.location.y += 1;
-                    agent.location.x -= 1;
-                    break;
-                case "s":
-                    direction = "s";
-                    agent.location.y += 1;
-                    break;
-                case "se":
-                    direction = "se";
-                    agent.location.y += 1;
-                    agent.location.x += 1;
-                    break;
-                default:
-                    return BadRequest("Invalid direction specified.");
-            }
+            DirectionService.DirectionActions[direction](agent.location);
             this._context.agents.Update(agent);
             await this._context.SaveChangesAsync();
             status = StatusCodes.Status200OK;
---
+++ Services/MoveDict.cs
@@ -0,0 +1,19 @@
+﻿using MossadAgentAPI.Models;
+
+namespace MossadAgentAPI.Services
+{
+    public static class DirectionService
+    {
+        public static readonly Dictionary<string, Action<Location>> DirectionActions = new Dictionary<string, Action<Location>>
+        {
+            { "nw", loc => { loc.y -= 1; loc.x -= 1; } },
+            { "n", loc => loc.y -= 1 },
+            { "ne", loc => { loc.y -= 1; loc.x += 1; } },
+            { "w", loc => loc.x -= 1 },
+            { "e", loc => loc.x += 1 },
+            { "sw", loc => { loc.y += 1; loc.x -= 1; } },
+            { "s", loc => loc.y += 1 },
+            { "se", loc => { loc.y += 1; loc.x += 1; } }
+        };
+    }
+}
